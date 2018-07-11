d454ff9a874fec87f392168eb169edc0d1bc37e7
Alena Pahuda <Alena_Pahuda@epam.com>
2018-07-11 15:16:07 +0300

    something begin too work

--- features/pages/page.js
+++ features/pages/page.js
@@ -1,66 +1,58 @@
-// spec.js
-
 class Page {
 
     get() {
         return browser.get('https://edit.weather.com/');
     }
 
-    searchMyCity() {
-        return browser.actions().mouseMove(by.id("topNav1"));
+    clickOnMaps() {
+        return browser.findElement(by.xpath("//div[@id='mini-panel-header_drilldown_mini_panel']/div/div/div/div/ul/li[2]/a/span")).click();
     }
 
-    clickMaps() {
-        return browser.findElement(by.linkText("B&N Top 100")).click();
-    }
+    // pushOnWeather() {
+    //     return browser.findElement(by.linkText("Weather in Motion®")).click();
+    // }
 
-    closeAdd() {
-        return browser.findElement(by.className("icon-close-modal")).click();
-    }
+    // pressPause() {
+    //     return browser.findElement(by.xpath("//div[@id='wx-main']/container-map/div/div/section/p-map/p-map-animation-timeline/animation-controls/button")).click();
+    // }
 
-    addBookToCart() {
-        browser.findElement(by.xpath("//input[@value='PRE-ORDER']")).click();
-        return browser.findElement(by.css("a.icon-close-modal")).click();
-    }
+    // clickBookLink(number) {
+    //     switch (number) {
+    //         case "first": {
+    //             return browser.findElement(by.xpath("//a[contains(text(),'An Absolutely Remarkable Thing (Signed B&N Exclusive Book)')]")).click();
+    //             break;
+    //         }
+    //         case "second": {
+    //             return browser.findElement(by.xpath("//a[contains(text(),'The Hate U Give')]")).click();
+    //             break;
+    //         }
+    //         case "third" : {
+    //             return browser.findElement(by.xpath("//a[contains(text(),'The President Is Missing')]")).click();
+    //             break;
+    //         }
+    //
+    //     }
+    //
+    // }
+    //
+    // scrollToBook(number) {
+    //     switch (number) {
+    //         case "first": {
+    //             break;
+    //         }
+    //         case "second": {
+    //             return browser.executeScript(`window.scrollTo(100,100);`);
+    //             break;
+    //         }
+    //         case "third" : {
+    //             return browser.executeScript(`window.scrollTo(500,500);`);
+    //             break;
+    //         }
+    //     }
+    // }
 
-    clickBookLink(number) {
-        switch (number) {
-            case "first": {
-                return browser.findElement(by.xpath("//a[contains(text(),'An Absolutely Remarkable Thing (Signed B&N Exclusive Book)')]")).click();
-                break;
-            }
-            case "second": {
-                return browser.findElement(by.xpath("//a[contains(text(),'The Hate U Give')]")).click();
-                break;
-            }
-            case "third" : {
-                return browser.findElement(by.xpath("//a[contains(text(),'The President Is Missing')]")).click();
-                break;
-            }
-
-        }
-
-    }
-
-    scrollToBook(number) {
-        switch (number) {
-            case "first": {
-                break;
-            }
-            case "second": {
-                return browser.executeScript(`window.scrollTo(100,100);`);
-                break;
-            }
-            case "third" : {
-                return browser.executeScript(`window.scrollTo(500,500);`);
-                break;
-            }
-        }
-    }
-
-    highlightOldPriceAndPercent() {
-        this.highlightWithJS(element(by.css("span.saved-percent.discount-amount")));
-        return this.highlightWithJS(element(by.css("s.old-price")));
+    highlightLocation() {
+        return this.highlightWithJS(element(by.css("span.location-text.not-truncated")));
     }
 
     highlightWithJS(el) {
@@ -77,10 +69,6 @@ class Page {
         }).then(function () {
             return browser.sleep(1000);
         })
-    }
-
-    getNewPrice() {
-        return element(by.id("pdp-cur-price")).getText();
     }
 }
 
--- features/steps/bookshop_steps.js
+++ features/steps/bookshop_steps.js
@@ -6,42 +6,27 @@ module.exports = function() {
 
     const page = new Page();
 
-    this.Given(/^The main page is open$/, { timeout: 70 * 1000 }, (callback) => {
+    this.Given(/^The main page is open$/, { timeout: 700 * 1000 }, (callback) => {
         page.get().then(callback);
     });
 
-    this.Given(/^Stupid add is close$/, { timeout: 70 * 1000 }, (callback) => {
-        page.closeAdd().then(callback);
+    this.When(/^I see my location$/, { timeout: 700 * 1000 }, (callback) => {
+        page.highlightLocation().then(callback);
     });
 
-    this.When(/^I move to books link$/, { timeout: 70 * 1000 }, (callback) => {
-        page.goToBooksLink().then(callback);
+    this.When(/^I click on maps link$/, { timeout: 700 * 1000 }, () => {
+        page.clickOnMaps();
     });
 
-    this.When(/^I click on top 100 link$/, { timeout: 30 * 1000 }, (callback) => {
-        page.clickTop100Link().then(callback);
-    });
+    // this.When(/^I push on Weather in Motion link$/,{ timeout: 700 * 1000 }, () => {
+    //     page.pushOnWeather();
+    // });
+    // this.When(/^I press pause$/,{ timeout: 700 * 1000 }, () => {
+    //     page.pressPause();
+    // });
 
-    this.When(/^I scroll down to "([^"]*)" book$/, (bookNumber) => {
-        page.scrollToBook(bookNumber);
-    });
-
-    this.When(/^I open book link$/, (bookNumber, callback) => {
-        page.clickBookLink(bookNumber).then(callback);
-    });
-
-    this.When(/^I add book to my shopping cart$/, (callback) => {
-        page.addBookToCart().then(callback);
-    })
-
-    this.When(/^I should see old prices and % I save$/, (callback) => {
-        page.highlightOldPriceAndPercent().then(callback);
-    });
-
-    this.Then(/^I should see that new price is "([^"]*)"$/, (expectedPrice, callback) => {
-        page.getNewPrice().then((text) => {
-            expect(text).to.eql(expectedPrice);
-        }).then(callback);
+    this.Then(/^I should see map$/, () => {
+       browser.sleep(20000);
     });
 
 };
